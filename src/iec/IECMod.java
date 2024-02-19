a0c2ab3a54ba58a67b121aad0cfa176e1d24ad63
Francuz <160481609+Fr4ncuzS4n@users.noreply.github.com>
2024-02-19 17:06:32 +0700

    Update and rename ExampleJavaMod.java to IECMod.java

--- src/iec/IECMod.java
+++ src/iec/IECMod.java
@@ -1,4 +1,4 @@
-package example;
+package iec;
 
 import arc.*;
 import arc.util.*;
@@ -9,11 +9,9 @@ import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class IECMod extends Mod{
 
-    public ExampleJavaMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
-
+    public IECMod(){
         //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
             //show dialog upon startup
@@ -21,7 +19,7 @@ public class ExampleJavaMod extends Mod{
                 BaseDialog dialog = new BaseDialog("frog");
                 dialog.cont.add("behold").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+                dialog.cont.image(Core.atlas.find("iec-frog")).pad(20f).row();
                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
