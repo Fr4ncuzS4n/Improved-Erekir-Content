5cecf5fe180c6e7bf2c1d549e6abc7a448c9c882
Fr4ncuz <fr4ncuz.s4n@gmail.com>
2024-04-03 17:27:45 +0700

    delete unnecessary code

--- src/iec/world/power/IECHeaterGenerator.java
+++ src/iec/world/power/IECHeaterGenerator.java
@@ -2,9 +2,6 @@ package iec.world.power;
 
 import arc.Core;
 import arc.math.Mathf;
-import arc.util.Time;
-import arc.util.io.Reads;
-import arc.util.io.Writes;
 import mindustry.graphics.Pal;
 import mindustry.ui.Bar;
 import mindustry.world.blocks.heat.HeatConsumer;
@@ -29,75 +26,5 @@ public class IECHeaterGenerator extends VariableReactor {
                         () -> entity.heat / maxHeat));
     }
 
-    public class IECHeaterGeneratorBuild extends VariableReactorBuild implements HeatConsumer{
-        public float[] sideHeat = new float[4];
-        public float heat = 0f, totalProgress, warmup;
-
-        @Override
-        public void updateTile(){
-            heat = calculateHeat(sideHeat);
-
-            productionEfficiency = efficiency;
-            warmup = Mathf.lerpDelta(warmup, productionEfficiency > 0 ? 1f : 0f, warmupSpeed);
-
-            totalProgress += productionEfficiency * Time.delta;
-
-            if(Mathf.chanceDelta(effectChance * warmup)){
-                effect.at(x, y, effectColor);
-            }
-        }
-
-        @Override
-        public void draw(){
-            super.draw();
-
-        }
-
-        @Override
-        public float totalProgress(){
-            return totalProgress;
-        }
-
-        @Override
-        public float warmup(){
-            return warmup;
-        }
-
-        @Override
-        public void updateEfficiencyMultiplier(){
-            //at this stage efficiency = how much coolant is provided
-
-            //target efficiency value
-            float target = Mathf.clamp(heat / maxHeat);
-
-            //now scale efficiency by target, so it consumes less depending on heat
-            efficiency *= target;
-        }
-
-        @Override
-        public float[] sideHeat(){
-            return sideHeat;
-        }
-
-        @Override
-        public float heatRequirement(){
-            return maxHeat;
-        }
-
-        @Override
-        public void write(Writes write){
-            super.write(write);
-
-            write.f(heat);
-            write.f(warmup);
-        }
-
-        @Override
-        public void read(Reads read, byte revision){
-            super.read(read, revision);
-
-            heat = read.f();
-            warmup = read.f();
-        }
-    }
+    public class IECHeaterGeneratorBuild extends VariableReactorBuild implements HeatConsumer{ }
 }
